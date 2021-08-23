c64791871cf815331c71459b282dd02197053613
Amir Shojae <amirshojae@alphafx.co.uk>
2021-08-23 15:49:45 +0100

    created User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,23 @@
+/* eslint-disable no-unused-vars */
 import 'reflect-metadata';
+import { Field, ID, ObjectType } from 'type-graphql';
 
-type User = {
+interface UserType {
     id: string,
     name: string,
+    accounts: string[],
 }
+
+@ObjectType()
+class User implements UserType {
+    @Field((type) => ID)
+    id!: string;
+
+    @Field((type) => String)
+    name!: string;
+
+    @Field((type) => [String], { nullable: true })
+    accounts!: string[];
+}
+
+export default User;
